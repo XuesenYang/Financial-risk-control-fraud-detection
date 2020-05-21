114da26d8f5ee5019c96969ecb97dfd142cf2135
XuesenYang <42894881+XuesenYang@users.noreply.github.com>
2020-05-21 21:17:46 +0800

    Update XGBoost+KS值特征选择.py

--- example/XGBoost+KS值特征选择.py
+++ example/XGBoost+KS值特征选择.py
@@ -1,37 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 24 15:08:42 2019
-
-@author: zixing.mei
-"""
 import math
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 import numpy as np
+from sklearn.metrics import roc_curve
+
 
 # 计算KS值，kS值可以描述模型对于正负例的区分能力
-def sloveKS(model, X, Y, Weight):
-    Y_predict = [s[1] for s in model.predict_proba(X)]  
-    nrows = X.shape[0]  
-    #还原权重  
-    lis = [(Y_predict[i], Y.values[i], Weight.values[i]) for i in range(nrows)]
-    #按照预测概率倒序排列  
-    ks_lis = sorted(lis, key=lambda x: x[0], reverse=True)        
-    KS = list()  
-    bad = sum([w for (p, y, w) in ks_lis if y > 0.5])  
-    good = sum([w for (p, y, w) in ks_lis if y <= 0.5])  
-    bad_cnt, good_cnt = 0, 0  
-    for (p, y, w) in ks_lis:  
-        if y > 0.5:  
-            #1*w 即加权样本个数  
-            bad_cnt += w                
-        else:  
-            #1*w 即加权样本个数  
-            good_cnt += w               
-        ks = math.fabs((bad_cnt/bad)-(good_cnt/good))  
-        KS.append(ks)  
-    return max(KS) 
+def sloveKS(model, X, Y):
+    """
+    :param model: 预测模型
+    :param X: 特征
+    :param Y: 真实标签
+    :return: KS值
+    """
+    Y_predict = [s[1] for s in model.predict_proba(X)]
+    fpr, tpr, thresholds = roc_curve(Y, Y_predict)
+    ks = max(tpr - fpr)
+    return ks
 
 # 计算PSI值， 稳定度指标(population stability index ,PSI)可衡量测试样本及模型开发样本评分的的分布差异
 def slovePSI(model, train_x, val_x):
@@ -60,15 +46,13 @@ def slovePSI(model, train_x, val_x):
     return PSI
   
 class feature_selection_model(object):
-    def __init__(self, datasets, uid, dep, weight,
+    def __init__(self, datasets, uid, dep,
                  var_names,  max_del_var_nums=0):
         self.datasets = datasets  
         #样本唯一标识，不参与建模  
         self.uid = uid       
         #二分类标签  
-        self.dep = dep     
-        #样本权重  
-        self.weight = weight      
+        self.dep = dep
         #特征列表  
         self.var_names = var_names
         #单次迭代最多删除特征的个数  
@@ -76,7 +60,7 @@ class feature_selection_model(object):
         self.row_num = 0  
         self.col_num = 0  
   
-    def training(self, min_score=0.0001, modelfile="", output_scores=list()):  
+    def training(self, min_score=0.0001):
         lis = self.var_names[:]
         orthers, test_data = train_test_split(self.datasets, test_size=0.2)  # 测试集
         train_data, val_data = train_test_split(orthers, test_size=0.3)  # 训练集，验证集
@@ -104,17 +88,17 @@ class feature_selection_model(object):
                     break  
             #训练集KS  
             trainks = sloveKS(model, train_data[self.var_names],
-                              train_data[self.dep], train_data[self.weight])
+                              train_data[self.dep])
             #初始化ks值和PSI  
             valks, testks, valpsi, testpsi = 0.0, 0.0, 0.0, 0.0 
             #测试集KS和PSI  
             if not isinstance(val_data, str):  
                 valks = sloveKS(model, val_data[self.var_names],
-                                     val_data[self.dep], val_data[self.weight])
+                                     val_data[self.dep])
                 valpsi = slovePSI(model, train_data[self.var_names], val_data[self.var_names])
             #跨时间验证集KS和PSI  
             if not isinstance(test_data, str):  
-                testks = sloveKS(model, test_data[self.var_names],test_data[self.dep], test_data[self.weight])
+                testks = sloveKS(model, test_data[self.var_names],test_data[self.dep])
                 testpsi = slovePSI(model, train_data[self.var_names], test_data[self.var_names])
             #将三个数据集的KS和PSI放入字典  
             dic = {"train_ks": float(trainks), "val_ks": float(valks), "test_ks": testks,
@@ -133,11 +117,8 @@ if __name__ == '__main__':
         data[j].loc[data[j] == '~'] = 0
     data.fillna(0, inplace=True)
 
-    Positive_sampling_scale = np.sum(data['flag'])/data.shape[0]
-    Nagetive_sampling_scale = 1 - Positive_sampling_scale
-    data['weight'] = [1/Positive_sampling_scale if x==1 else 1/Nagetive_sampling_scale for x in data['flag']]
     features_names = data.columns.values.tolist()
-    fs_model =  feature_selection_model(datasets = data, uid = 'id', dep = 'flag', weight = 'weight',
-                 var_names = data.columns.values.tolist()[8:], max_del_var_nums=4)
-    feature_subset = fs_model.training()
+    fs_model =  feature_selection_model(datasets = data, uid = 'id', dep = 'flag',
+                 var_names = data.columns.values.tolist()[8:], max_del_var_nums=5)
+    feature_subset = fs_model.training( min_score=0.001)
     print(feature_subset)
